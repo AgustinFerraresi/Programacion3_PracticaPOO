5926b477b4cb2a00c2a882262bfd6f97accf7bd8
Agustin <agustinferraresi5@gmail.com>
2024-08-21 15:15:05 -0300

    ej1 POO resuelto

---
+++ src/Web/Controllers/Ej1Controller.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Web.Ej1;
+namespace Web.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ControllerEj1 : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get([FromQuery] List<string> names)
+    {
+        var person1 = new Person(names[0]);
+        var person2 = new Person(names[1]);
+        var person3 = new Person(names[2]);
+
+        List<Person> personas = new List<Person> { person1, person2, person3};
+
+        var saludos = personas.Select(persona => persona.GetSaludo()).ToList();
+
+
+    
+        return Ok(saludos);
+    }
+}
---
+++ src/Web/Ej1/Person.cs
@@ -0,0 +1,18 @@
+﻿namespace Web.Ej1
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        //constructor
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        public string GetSaludo() 
+        {
+            return $"Hola! mi nombre es {Name}";
+        }
+    }
+}
